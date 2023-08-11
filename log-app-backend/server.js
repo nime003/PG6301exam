9d7a7ed4369f93d6f20405f88a3320c6d5f1d1e1
nime003 <nime003@student.kristiania.no>
2023-08-11 05:16:15 +0200

    I tried to make a fat jar for deployment but it didnt work

--- log-app-backend/server.js
+++ log-app-backend/server.js
@@ -94,6 +94,12 @@ app.post('/logHours', (req, res) => {
     }
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
